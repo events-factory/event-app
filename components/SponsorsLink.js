f00729636c7ca9c34fc6a1a041acf5a1a429348c
Theophile-Futurix <theophile.futurix.co>
2020-10-25 09:58:07 +0200

    Add Linking to the sponsorsLinks page

--- components/SponsorsLink.js
+++ components/SponsorsLink.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, Linking } from "react-native";
 
 const SponsorsLink = ({ style }) => {
   return (
